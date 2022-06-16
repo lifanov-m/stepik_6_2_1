20dab52e8607e40aedffd8e9e103ec487dfe7bdd
Lifanov <lifanov-m@mail.ru>
2022-06-16 17:02:07 +0300

    Three versions of method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -18,17 +19,69 @@ public class Main {
         set2.add(5);
         set2.add(6);
 
-        Set<Integer> result = symmetricDifference(set1, set2);
+        Set<Integer> result = symmetricDifference_v1(set1, set2);
+        System.out.println( "result for v1:  " + result );
 
-        System.out.println( result );
+        result = symmetricDifference_v2(set1, set2);
+        System.out.println( "result for v2:  " + result );
     }
 
-    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+    public static <T> Set<T> symmetricDifference_v1(Set<? extends T> set1, Set<? extends T> set2) {
 
         Set<T> result = new LinkedHashSet<>();
+
+        for (T el:set1
+             ) {
+            if(! set2.contains(el)) {
+                result.add(el);
+            }
+        }
+
+        for (T el:set2
+        ) {
+            if(! set1.contains(el)) {
+                result.add(el);
+            }
+        }
 
 
         return result;
     }
 
+    public static <T> Set<T> symmetricDifference_v2(Set<? extends T> set1, Set<? extends T> set2) {
+
+        Set<T> result = new LinkedHashSet<>();
+
+        Set<? extends T> set1_copy = new HashSet<>(set1);
+        Set<? extends T> set2_copy = new HashSet<>(set2);
+
+        Set<? extends T> sum = set1_copy.addAll(set2_copy);
+
+
+        return result;
+    }
+
+
+    public static <T> Set<T> symmetricDifference_v3(Set<? extends T> set1, Set<? extends T> set2) {
+
+        Set<T> result = new LinkedHashSet<>();
+        /*
+        Iterator<T> iterator = (Iterator<T>) set1.iterator();
+
+        while (iterator.hasNext()) {
+            if(! set2.contains(iterator.next())) {
+                result.add(iterator.next());
+            }
+        }
+
+        for (T el:set2
+        ) {
+            if(! set1.contains(el)) {
+                result.add(el);
+            }
+        }
+
+        */
+        return result;
+    }
 }
